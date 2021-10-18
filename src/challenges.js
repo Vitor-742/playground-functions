03902a96eb796b3f0c1759e4da45fe2c6bba69f8
Vitor Campos <vitorcampos742@gmail.com>
2021-10-18 15:08:18 -0300

    desafios 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  //aqui
+function compareTrue(p1, p2) {
+  if (p1 == true && p2 == true) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base*height)/2
+  return area
 }
 
 // Desafio 3
