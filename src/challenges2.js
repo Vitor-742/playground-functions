b127d060366b2def8d1b11b76e089e6dee2eab8a
Vitor Campos <vitorcampos742@gmail.com>
2021-10-18 19:27:28 -0300

    mais detalhes

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,7 +7,7 @@ function techList(arrTec, name) {
   let arr2 = []
   for (let i in arrTec) {
     arr2[i] = {tech: arrTec[i], 'name': name}
-  };
+  }
   return arr2;
 }
 
@@ -22,8 +22,8 @@ function generatePhoneNumber(p1) {
     for (let i2 in p1) {
       if (p1[i] == p1[i2]) {
         cont += 1
-      };
-    };
+      }
+    }
   if (p1[i] < 0 || p1[i] > 9 || cont >= 3) {
     return "não é possível gerar um número de telefone com esses valores"
   };
@@ -38,8 +38,8 @@ function triangleCheck(lineA, lineB, lineC) {
     return true
   } else {
     return false
-  };
-};
+  }
+}
 
 // Desafio 13
 function hydrate(p1) {
@@ -48,14 +48,14 @@ function hydrate(p1) {
   for (let i in arr) {
     if (!isNaN(arr[i])){
       soma += parseInt(arr[i])
-    };
-  };
+    }
+  }
   if (soma == 1) {
     return soma + ' copo de água';
   } else {
     return soma + ' copos de água';
-};
-};
+}
+}
 
 module.exports = {
   generatePhoneNumber,
